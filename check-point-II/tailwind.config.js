8cfa16a410a7a652f3c0531ff343dfc064c154c1
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-06-27 23:03:01 -0300

    style: redefinido style guide no tailwind.config

--- check-point-II/tailwind.config.js
+++ check-point-II/tailwind.config.js
@@ -5,8 +5,39 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "",
+        fontColor: "#202329",
+        secundary: "#FFFFFF",
+        greyColor: "#08C952",
+        aliveColor: "",
+        deadColor: "#A1140A",
+        unknowColor: "#A6A6A6",
+        buttonBg: "#A1140A",
+        starColor: "#FCE46D",
+      },
+      fonts: {
+        h1Font: {
+          fontWeight: "bold",
+          fontSize: "2rem",
+        },
+        h2Font: {
+          fontWeight: "semibold",
+          fontSize: "1.5rem",
+        },
+        h3Font: {
+          fontWeight: "bold",
+          fontSize: "2rem",
+        },
+        pFont: "",
+        buttonFont: "",
+
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
-
